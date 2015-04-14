0893003e4042794aa5408be0e26569a8e7018412
Ihor <serkov.ihor@gmail.com>
2015-04-14 14:56:10 +0300

    sysadmin area

---
+++ PmaPlus.Model/Enums/AgeGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PmaPlus.Model.Enums
+{
+    public enum AgeGroup
+    {
+        U6 = 0,
+        U7 = 1,
+        U8 = 2,
+        U9 = 3,
+        U10 = 4,
+        U11 = 5,
+        U12 = 6,
+        U13 = 7,
+        U14 = 8,
+        U15 = 9,
+        U16 = 10,
+        U17 = 11,
+        U18 = 12,
+        P18 = 13
+    }
+}
---
+++ PmaPlus.Model/Enums/CertificateCourseType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PmaPlus.Model.Enums
+{
+    public enum CertificateCourseType
+    {
+        FA = 0,
+        CRB_DBS = 1,
+        FirstAid = 2,
+        Professional = 3,
+        Safeguarding = 4,
+        Other = 5
+    }
+}
