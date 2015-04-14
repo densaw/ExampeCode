b1daf423ee728624320408dace31ef5a5d15022d
Ihor <serkov.ihor@gmail.com>
2015-04-14 19:29:14 +0300

    enum

---
+++ PmaPlus.Model/Enums/MatchType.cs
@@ -0,0 +1,11 @@
+﻿namespace PmaPlus.Model
+{
+    public enum MatchType
+    {
+        Cup = 0,
+        Friendly = 1,
+        International = 2,
+        League = 3,
+        Tournament = 4
+    }
+}
