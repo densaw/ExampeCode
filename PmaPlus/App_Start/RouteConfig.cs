aee3d4118bc1ff47be450b4b0494b03c53aa3aaf
Upee <iwebsheep@live.ru>
2015-04-15 11:16:56 +0300

    MyCommit

--- PmaPlus/App_Start/RouteConfig.cs
+++ PmaPlus/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace PmaPlus
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new {  action = "Index", controller = "Home", id = UrlParameter.Optional }
+                defaults: new {  action = "Index", controller = "Home", id = UrlParameter.Optional },
+                namespaces: new[] { "PmaPlus.Controllers"}
             );
         }
     }
---
+++ PmaPlus/Areas/SysAdmin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PmaPlus.Model;\
+
+namespace PmaPlus.Areas.SysAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: SysAdmin/Home
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+    }
+}
