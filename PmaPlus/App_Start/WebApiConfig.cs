85fabea37f02108c212b5f86e29797c22c290182
Ihor <serkov.ihor@gmail.com>
2015-04-17 12:30:00 +0300

    Camal case api

--- PmaPlus/App_Start/WebApiConfig.cs
+++ PmaPlus/App_Start/WebApiConfig.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace PmaPlus
 {
@@ -21,6 +23,8 @@ namespace PmaPlus
                 defaults: new { id = RouteParameter.Optional }
             );
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
         }
     }
 }
