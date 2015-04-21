2d0e55bfdd57c5ff29f60597aa883222c61be55d
Ihor <serkov.ihor@gmail.com>
2015-04-21 11:59:52 +0300

    dbmigr

---
+++ PmaPlus/Mapping/ViewModelToModelProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PmaPlus.Model.Models;
+using PmaPlus.Model.ViewModels.Curriculum;
+
+namespace PmaPlus.Mapping
+{
+    class ViewModelToModelProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToModelProfile"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<CurriculumTypeViewModel, CurriculumType>();
+
+
+        }
+    }
+}
