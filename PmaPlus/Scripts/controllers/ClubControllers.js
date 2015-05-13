19eedb00c4d186bbf17123274a66830a57633014
Upee <iwebsheep@live.ru>
2015-05-13 15:59:31 +0300

    Calendar View

--- PmaPlus/Scripts/controllers/ClubControllers.js
+++ PmaPlus/Scripts/controllers/ClubControllers.js
@@ -263,3 +263,95 @@ app.controller('ToDoController', ['$scope', '$http', 'toaster', function($scope,
     }
 
 }]);
+
+app.controller('ClubDiaryController', ['$scope', '$http', 'toaster', '$compile', 'uiCalendarConfig', function ($scope, $http, toaster, $compile, uiCalendarConfig) {
+
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    console.log(date);
+
+    $scope.events = [
+      { title: 'All Day Event', start: new Date(y, m, 1) },
+      { title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2) },
+      { id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false },
+      { id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false },
+      { title: 'Birthday Party', start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false }
+    ];
+
+
+    $scope.alertOnEventClick = function (date, jsEvent, view) {
+        $scope.alertMessage = (date.title + ' was clicked ');
+    };
+    /* alert on Drop */
+    $scope.alertOnDrop = function (event, delta, revertFunc, jsEvent, ui, view) {
+        $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
+    };
+    /* alert on Resize */
+    $scope.alertOnResize = function (event, delta, revertFunc, jsEvent, ui, view) {
+        $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
+    };
+
+    $scope.eventRender = function (event, element, view) {
+        element.attr({
+            'tooltip': event.title,
+            'tooltip-append-to-body': true
+        });
+        $compile(element)($scope);
+    };
+
+    /* add and removes an event source of choice */
+    $scope.addRemoveEventSource = function (sources, source) {
+        var canAdd = 0;
+        angular.forEach(sources, function (value, key) {
+            if (sources[key] === source) {
+                sources.splice(key, 1);
+                canAdd = 1;
+            }
+        });
+        if (canAdd === 0) {
+            sources.push(source);
+        }
+    };
+    /* add custom event*/
+    $scope.addEvent = function () {
+        $scope.events.push({
+            title: 'Open Sesame',
+            start: new Date(y, m, 28),
+            end: new Date(y, m, 29),
+            className: ['openSesame']
+        });
+    };
+    /* remove event */
+    $scope.remove = function (index) {
+        $scope.events.splice(index, 1);
+    };
+    /* Change View */
+    $scope.changeView = function (view, calendar) {
+        uiCalendarConfig.calendars[calendar].fullCalendar('changeView', view);
+    };
+    /* Change View */
+    $scope.renderCalender = function (calendar) {
+        if (uiCalendarConfig.calendars[calendar]) {
+            uiCalendarConfig.calendars[calendar].fullCalendar('render');
+        }
+    };
+
+    $scope.uiConfig = {
+        calendar: {
+            height: 450,
+            editable: true,
+            header: {
+                left: 'month basicWeek basicDay agendaWeek agendaDay',
+                center: 'title',
+                right: 'today prev,next'
+            },
+            eventClick: $scope.alertOnEventClick,
+            eventDrop: $scope.alertOnDrop,
+            eventResize: $scope.alertOnResize,
+            eventRender: $scope.eventRender
+        }
+    };
+}]);
