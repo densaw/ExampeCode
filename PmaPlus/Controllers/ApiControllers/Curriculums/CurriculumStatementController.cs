226f97de192723022d5c58bddb9bced50c257c61
Ihor <serkov.ihor@gmail.com>
2015-05-22 16:10:39 +0300

    sddds

--- PmaPlus/Controllers/ApiControllers/Curriculums/CurriculumStatementController.cs
+++ PmaPlus/Controllers/ApiControllers/Curriculums/CurriculumStatementController.cs
@@ -69,6 +69,9 @@ namespace PmaPlus.Controllers.ApiControllers.Curriculums
             return Ok();
         }
 
+
+
+
         public IHttpActionResult Delete(int id)
         {
             if (!_curriculumServices.StatementExist(id))
