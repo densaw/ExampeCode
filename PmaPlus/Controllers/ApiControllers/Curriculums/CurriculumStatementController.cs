37dbee149171656c5ac3130ef529a5aeceeb590c
Ihor <serkov.ihor@gmail.com>
2015-05-25 15:43:59 +0300

    do to

--- PmaPlus/Controllers/ApiControllers/Curriculums/CurriculumStatementController.cs
+++ PmaPlus/Controllers/ApiControllers/Curriculums/CurriculumStatementController.cs
@@ -74,6 +74,10 @@ namespace PmaPlus.Controllers.ApiControllers.Curriculums
 
         public IHttpActionResult Put(int id, [FromBody] CurriculumStatementViewModel statementViewModel)
         {
+
+            var statement = Mapper.Map<CurriculumStatementViewModel, CurriculumStatement>(statementViewModel);
+
+            _curriculumServices.UpdateCurriculumStatment(statement, statementViewModel.Roles, _userServices.GetClubAdminByUserName(User.Identity.Name).Club.Id);
             return Ok();
         }
 
