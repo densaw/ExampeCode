f970dc0cdb10349ffab536b649d32f9e8885547e
Denys Arkhipenko <denys5dev@gmail.com>
2015-05-26 17:59:30 +0300

    delete diary

--- PmaPlus/Scripts/controllers/ClubControllers.js
+++ PmaPlus/Scripts/controllers/ClubControllers.js
@@ -439,7 +439,10 @@ app.controller('ClubDiaryController', ['$scope', '$http', 'toaster', '$compile',
 
     var cal = angular.element('#calendar');
     var urlTail = '/api/Diary';
+
     
+
+
 
     cal.fullCalendar({
         header: {
@@ -453,7 +456,7 @@ app.controller('ClubDiaryController', ['$scope', '$http', 'toaster', '$compile',
 
             //var allDay = !start.hasTime() && !end.hasTime();
             $scope.newEvent.start = moment(start).format();
-            $('#addDiaryModal').modal();//open the modal
+            target.modal('show');//open the modal
             //alert(["Event Start date: " + moment(start).format(),
                    //"Event End date: " + moment(end).format(),
                    //"AllDay: " + allDay].join("\n"));
@@ -488,6 +491,7 @@ app.controller('ClubDiaryController', ['$scope', '$http', 'toaster', '$compile',
                     .success(function(result) {
                         $scope.newEvent = result;
                         needToUpdate = event.id;
+                        needToDelete = event.id;
                         $scope.modalTitle = "Edit Event";
                         target.modal('show');
                         console.log('done');
@@ -498,8 +502,12 @@ app.controller('ClubDiaryController', ['$scope', '$http', 'toaster', '$compile',
         }
     });
 
+    $scope.Evnotes = [];
+    $http.get('/api/Diary/').success(function (result) {
+        $scope.Evnotes = result;
+    });
+    var confDelete = angular.element('#confDelete');
 
-    
     function getResults() {
         $http.get(urlTail)
             .success(function (result) {
@@ -568,8 +576,15 @@ app.controller('ClubDiaryController', ['$scope', '$http', 'toaster', '$compile',
         needToDelete = -1;
         getResults();
         target.modal('hide');
-        deleteConf.modal('hide');
+        
     }
+    $scope.delete = function () {
+        $http.delete(urlTail + '/' + needToDelete).success(function () {
+            //getResults();
+            needToDelete = -1;
+            target.modal('hide');
+        });
+    };
 
 
 }]);
