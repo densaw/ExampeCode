e13e0c7887fd1d62f77c1643e84da62c2629ac28
Ihor <serkov.ihor@gmail.com>
2015-05-29 16:17:35 +0300

    news pages done

--- PmaPlus/Controllers/ApiControllers/ExerciseNewsController.cs
+++ PmaPlus/Controllers/ApiControllers/ExerciseNewsController.cs
@@ -10,16 +10,19 @@ using PmaPlus.Model.Models;
 using PmaPlus.Model.ViewModels.News;
 using PmaPlus.Model.ViewModels.Nutrition;
 using PmaPlus.Services.Services;
+using PmaPlus.Tools;
 
 namespace PmaPlus.Controllers.ApiControllers
 {
     public class ExerciseNewsController : ApiController
     {
         private readonly NewsServices _newsServices;
+        private readonly IPhotoManager _photoManager;
 
-        public ExerciseNewsController(NewsServices newsServices)
+        public ExerciseNewsController(NewsServices newsServices, IPhotoManager photoManager)
         {
             _newsServices = newsServices;
+            _photoManager = photoManager;
         }
 
         [Route("api/ExerciseNews/{pageSize:int}/{pageNumber:int}/{orderBy:alpha?}")]
@@ -40,6 +43,97 @@ namespace PmaPlus.Controllers.ApiControllers
         }
 
 
+        public ExerciseNewViewModel Get(int id)
+        {
+            return Mapper.Map<ExcerciseNew, ExerciseNewViewModel>(_newsServices.GetExcerciseNewById(id));
+        }
+
+        public IHttpActionResult Post([FromBody] ExerciseNewViewModel exerciseNewViewModel)
+        {
+            var exerciseNew =  _newsServices.AddExcerciseNew(Mapper.Map<ExerciseNewViewModel, ExcerciseNew>(exerciseNewViewModel));
+            if (exerciseNew != null)
+            {
+                if (_photoManager.FileExists(exerciseNew.AuthorPicture))
+                {
+                    exerciseNew.AuthorPicture = _photoManager.MoveFromTemp(exerciseNew.AuthorPicture,
+                        FileStorageTypes.ExerciseNews, exerciseNew.Id, "AuthorPicture");
+                }
+
+                if (_photoManager.FileExists(exerciseNew.MainPicture))
+                {
+                    exerciseNew.MainPicture = _photoManager.MoveFromTemp(exerciseNew.MainPicture,
+                        FileStorageTypes.ExerciseNews, exerciseNew.Id, "MainPicture");
+                }
+
+                if (_photoManager.FileExists(exerciseNew.SponsoredBy))
+                {
+                    exerciseNew.SponsoredBy = _photoManager.MoveFromTemp(exerciseNew.SponsoredBy,
+                        FileStorageTypes.ExerciseNews, exerciseNew.Id, "SponsoredBy");
+                }
+
+                if (_photoManager.FileExists(exerciseNew.Picture))
+                {
+                    exerciseNew.Picture = _photoManager.MoveFromTemp(exerciseNew.Picture,
+                        FileStorageTypes.ExerciseNews, exerciseNew.Id, "Picture");
+                }
+
+                _newsServices.UpdateExerciseNew(exerciseNew,exerciseNew.Id);
+
+
+            }
+            return Ok();
+
+        }
+
+
+        public IHttpActionResult Put(int id,[FromBody] ExerciseNewViewModel exerciseNewViewModel)
+        {
+            if (!_newsServices.ExerciseNewExist(id))
+            {
+                return NotFound();
+            }
+
+            var exerciseNew = Mapper.Map<ExerciseNewViewModel, ExcerciseNew>(exerciseNewViewModel);
+            
+            exerciseNew.Id = id;
+
+
+            if (_photoManager.FileExists(exerciseNew.AuthorPicture))
+            {
+                exerciseNew.AuthorPicture = _photoManager.MoveFromTemp(exerciseNew.AuthorPicture,
+                    FileStorageTypes.ExerciseNews, exerciseNew.Id, "AuthorPicture");
+            }
+
+            if (_photoManager.FileExists(exerciseNew.MainPicture))
+            {
+                exerciseNew.MainPicture = _photoManager.MoveFromTemp(exerciseNew.MainPicture,
+                    FileStorageTypes.ExerciseNews, exerciseNew.Id, "MainPicture");
+            }
+
+            if (_photoManager.FileExists(exerciseNew.SponsoredBy))
+            {
+                exerciseNew.SponsoredBy = _photoManager.MoveFromTemp(exerciseNew.SponsoredBy,
+                    FileStorageTypes.ExerciseNews, exerciseNew.Id, "SponsoredBy");
+            }
+
+            if (_photoManager.FileExists(exerciseNew.Picture))
+            {
+                exerciseNew.Picture = _photoManager.MoveFromTemp(exerciseNew.Picture,
+                    FileStorageTypes.ExerciseNews, exerciseNew.Id, "Picture");
+            }
+
+            _newsServices.UpdateExerciseNew(exerciseNew, exerciseNew.Id);
+
+            return Ok();
+
+
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            _newsServices.DeleteExcerciseNew(id);
+            return Ok();
+        }
 
     }
 }
