e743768a375e3e0cd574b56d5e35cd12518b0d8c
Ihor <serkov.ihor@gmail.com>
2015-06-02 10:45:56 +0300

    new area head

---
+++ PmaPlus/Areas/HeadOfAcademy/HeadOfAcademyAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PmaPlus.Areas.HeadOfAcademy
+{
+    public class HeadOfAcademyAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "HeadOfAcademy";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "HeadOfAcademy_default",
+                "HeadOfAcademy/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
