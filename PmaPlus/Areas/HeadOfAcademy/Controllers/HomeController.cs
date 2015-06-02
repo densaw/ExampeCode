d673cbe096ae7075104f85b062c0e6ae2abaf3c7
Ihor <serkov.ihor@gmail.com>
2015-06-02 10:47:59 +0300

    home for head

---
+++ PmaPlus/Areas/HeadOfAcademy/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PmaPlus.Areas.HeadOfAcademy.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HeadOfAcademy/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
