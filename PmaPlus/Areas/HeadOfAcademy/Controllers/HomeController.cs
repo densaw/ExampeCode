8107da1217f15413c6c3bd6f2fcf2c22e116b5cd
Денис <denia24528@gmail.com>
2015-06-02 16:08:18 +0300

    profile

--- PmaPlus/Areas/HeadOfAcademy/Controllers/HomeController.cs
+++ PmaPlus/Areas/HeadOfAcademy/Controllers/HomeController.cs
@@ -11,48 +11,52 @@ namespace PmaPlus.Areas.HeadOfAcademy.Controllers
     {
         // GET: HeadOfAcademy/Home
         public ActionResult Dashboard()
-        {
+        {            
             return View();
         }
+
         public ActionResult ClubDiary()
-        {
-            
+        {            
             return View();
         }
+
         public ActionResult Curriculums()
-        {
-            
+        {            
             return View();
         }
 
         public ActionResult Statements()
-        {
-           
+        {           
             return View();
         }
+
         public ActionResult Teams()
-        {
-           
+        {           
             return View();
         }
+
         public ActionResult ToDoList()
-        {
-           
+        {           
             return View();
         }
+
         public ActionResult Scenarios()
-        {
-           
+        {           
             return View();
         }
+
         public ActionResult SkillsAndKnowledge()
-        {
-            
+        {            
             return View();
         }
+
         public ActionResult Reports()
         {
+            return View();
+        }
 
+        public ActionResult Profile()
+        {
             return View();
         }
     }
