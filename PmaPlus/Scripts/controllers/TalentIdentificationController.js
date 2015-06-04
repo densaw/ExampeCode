719f508687ce569f469e1c2a029af5fab2782da2
Denys Arkhipenko <denys5dev@gmail.com>
2015-06-04 17:24:55 +0300

    TalentControl

---
+++ PmaPlus/Scripts/controllers/TalentIdentificationController.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('MainApp');
+
+app.controller('TalentIdentificationController', ['$scope', '$http', 'toaster', function ($scope, $http, toaster) {
+
+    var target = angular.element('#addTalent');
+
+
+    $scope.open = function () {
+        $scope.windowTitle = 'Add Scouted Player';
+        $scope.myform.form_Submitted = false;
+        target.modal('show');
+    }
+
+    $scope.cancel = function () {
+        target.modal('hide');
+        confDelete.modal('hide');
+    };
+
+}]);
