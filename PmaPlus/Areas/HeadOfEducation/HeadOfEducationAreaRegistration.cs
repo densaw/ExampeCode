78c87b12a879b39016f429e82ff83551ddae07ca
Denys Arkhipenko <denys5dev@gmail.com>
2015-06-04 11:16:07 +0300

    Scientist HoE

---
+++ PmaPlus/Areas/HeadOfEducation/HeadOfEducationAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PmaPlus.Areas.HeadOfEducation
+{
+    public class HeadOfEducationAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "HeadOfEducation";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "HeadOfEducation_default",
+                "HeadOfEducation/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
