205abd87a6566166f7b4bf0cff93e1ed7006a9b4
Ihor <serkov.ihor@gmail.com>
2015-06-08 17:56:08 +0300

    profile avatars

--- PmaPlus.Model/ViewModels/TalentIdentifications/TalentIdentificationViewModel.cs
+++ PmaPlus.Model/ViewModels/TalentIdentifications/TalentIdentificationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,25 @@ namespace PmaPlus.Model.ViewModels.TalentIdentifications
     public class TalentIdentificationViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
         public int ScoutId { get; set; }
         public DateTime DateIdentificated { get; set; }
+        [Required]
         public string PresentClub { get; set; }
+        [Required]
         public DateTime BirthDate { get; set; }
+        [Required]
         public string ParentsFirstName { get; set; }
+        [Required]
         public string ParentsLastName { get; set; }
+        [Required]
         public string ParentsEmail { get; set; }
+        [Required]
         public string ParentsMobile { get; set; }
+        [Required]
         public string ParentsNote { get; set; }
         
     }
---
+++ PmaPlus/Scripts/controllers/UserAvatarController.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module('MainApp');
+
+app.controller('UserAvatarController', ['$scope', '$http',  '$q', function($scope, $http, $q) {
+    
+
+    var getAvatar =function() {
+        $http.get("/api/Users/Avatar")
+            .success(function(result) {
+            $scope.avatar = result;
+        });
+    }
+
+
+    getAvatar();
+}]);
