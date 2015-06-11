76bf926b5ccd1bb1f04db73d8d4dfc2c11471254
Ihor <serkov.ihor@gmail.com>
2015-06-11 10:35:31 +0300

    wizart array

--- PmaPlus.Services/Services/CurriculumProcessServices.cs
+++ PmaPlus.Services/Services/CurriculumProcessServices.cs
@@ -25,18 +25,47 @@ namespace PmaPlus.Services.Services
         }
 
 
-        public IEnumerable<Session> GetTeamSessions(int teamId)
+        public IEnumerable<SessionsWizardViewModel> GetCurriculumSessionsWizard(int teamId)
         {
-            return _teamRepository.GetById(teamId).TeamCurriculum.Curriculum.Sessions.ToList();
+            var sessions = _teamRepository.GetById(teamId).TeamCurriculum.Curriculum.Sessions.ToList();
+            var sesResults = _teamRepository.GetById(teamId).TeamCurriculum.SessionResults;
+
+            var team = _teamRepository.GetById(teamId).TeamCurriculum.Id;
+
+
+            var result = from s in sessions
+
+                         join sr in sesResults on s.Id equals sr == null ? 0 : sr.SessionId into leftm
+                         from m in leftm.DefaultIfEmpty()
+
+                         select new SessionsWizardViewModel()
+                         {
+                             Attendance = s.Attendance,
+                             Name = s.Name,
+                             Rating = s.Rating,
+                             Number = s.Number,
+                             NeedScenarios = s.NeedScenarios,
+                             EndOfReviewPeriod = s.EndOfReviewPeriod,
+                             Done = m == null ? false : m.Done,
+                             ObjectiveReport = s.ObjectiveReport,
+                             Objectives = s.Objectives,
+                             Report = s.Report,
+                             CoachDetails = s.CoachDetails,
+                             CoachDetailsName = s.CoachDetailsName,
+                             CoachPicture = s.CoachPicture,
+                             PlayerDetails = s.PlayerDetails,
+                             PlayerDetailsName = s.PlayerDetailsName,
+                             PlayerPicture = s.PlayerPicture,
+                             StartOfReviewPeriod = s.StartOfReviewPeriod,
+                             StartedOn = m == null ? null : m.StartedOn,
+                             ComletedOn = m == null ? null : m.ComletedOn,
+                             SessionId = s.Id,
+                             TeamCurriculumId = team
+                         };
+            return result.OrderBy(s => s.Number);
         }
 
-        public IEnumerable<SessionResult> GetTeamSessionsResult(int teamId)
-        {
-            return _teamRepository.GetById(teamId).TeamCurriculum.SessionResults;
-        }
 
-
-        
 
 
 
