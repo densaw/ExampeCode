78f40d2b2380763e485dc036e711044a79c0163b
Denys Arkhipenko <denys5dev@gmail.com>
2015-06-18 17:33:51 +0300

    qqq

--- PmaPlus/Scripts/controllers/curriculumProcess/CurrAdendanceController.js
+++ PmaPlus/Scripts/controllers/curriculumProcess/CurrAdendanceController.js
@@ -3,14 +3,22 @@
 app.controller('AtendanceController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
 
     // get att step 2
+    var confDetail = angular.element('#confDetail');
+   
+    var confAtend = angular.element('#confAtend');
+
+
     var pathArray = $location.$$absUrl.split("/");
     $scope.currId = pathArray[pathArray.length - 1];
 
-    $http.get('/api request/' + $scope.currId)
+    $http.get('/api/Curriculum/Wizard/Session/AttendanceTable/')
         .success(function (result) {
             console.log(result);
             $scope.item = result;
 
         });
+    $scope.ssesionDetail = function () {
+        confDetail.modal('show');
+    };
 
 }]);
