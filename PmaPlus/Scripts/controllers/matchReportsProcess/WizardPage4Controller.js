1661501fb9250e61ee17289b28738b9aac030621
Denis <denia24528@gmail.com>
2015-06-29 11:48:21 +0300

    page4

---
+++ PmaPlus/Scripts/controllers/matchReportsProcess/WizardPage4Controller.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('MainApp');
+
+app.controller('WizardPage4Controller', ['$scope', '$http', '$q', '$location', '$rootScope', function ($scope, $http, $q, $location, $rootScope) {
+
+    var pathArray = $location.$$absUrl.split("/");
+    $scope.currId = pathArray[pathArray.length - 1];
+
+    var confDetail = angular.element('#confDetail');
+
+    $scope.openEdit = function () {
+        $scope.modalTitle = "Edit";
+        confDetail.modal('show');
+    };
+
+
+
+    $http.get('/api/MatchObjectives/' + $scope.currId).success(function (result) {
+        $scope.playersList = result;
+        console.log(result);
+    });
+
+}]);
