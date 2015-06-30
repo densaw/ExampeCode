624c72510c3585b04cf2c396327a10583e2d0812
Ihor <serkov.ihor@gmail.com>
2015-06-30 14:09:08 +0300

    users docs partial

---
+++ PmaPlus/Scripts/controllers/UsersDocumetsController.js
@@ -0,0 +1,35 @@
+﻿var app = angular.module('MainApp');
+
+app.controller('UsersDocumetsController', ['$scope', '$http', 'toaster', '$q', '$filter', function ($scope, $http, toaster, $q, $filter) {
+
+   
+
+    $scope.currentFolder = '';
+    var folderModal = angular.element('#addFolder');
+    var delModal = angular.element('#confDelete');
+
+    $scope.getFolders = function () {
+        $http.get('/api/Documents/Directories')
+         .success(function (data) {
+             $scope.folders = data;
+             if (data.length > 0) {
+                 $scope.getFiles(data[0].name);
+                 $scope.currentFolder = data[0].name;
+             }
+         });
+    };
+
+
+
+    $scope.getFiles = function (folderName) {
+        $http.get('/api/Documents/' + folderName)
+            .success(function (data) {
+                $scope.files = data;
+            });
+        $scope.currentFolder = folderName;
+    };
+
+
+    
+    $scope.getFolders();
+}]);
