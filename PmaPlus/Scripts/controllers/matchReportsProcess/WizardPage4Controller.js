10bb92a11dda73536ebf510a3fe0e457879036f4
Denys Arkhipenko <denys5dev@gmail.com>
2015-07-01 15:47:32 +0300

    qqq

--- PmaPlus/Scripts/controllers/matchReportsProcess/WizardPage4Controller.js
+++ PmaPlus/Scripts/controllers/matchReportsProcess/WizardPage4Controller.js
@@ -5,15 +5,13 @@ app.controller('WizardPage4Controller', ['$scope', '$http', '$q', '$location', '
     var pathArray = $location.$$absUrl.split("/");
     $scope.currId = pathArray[pathArray.length - 1];
 
-    var toggleInvitet = angular.element('#toggleMoMt');
+    
     
     var confInvitet = angular.element('#confInvitet');
     $scope.playerAdd = {};
 
     $scope.openEdit1 = function (player) {     
         $scope.player = player;
-        toggleInvitet.bootstrapToggle($scope.player.mom ? 'on' : 'off');
-        
         $scope.modalTitle = "Edit";
         confInvitet.modal('show');
     };
@@ -31,12 +29,12 @@ app.controller('WizardPage4Controller', ['$scope', '$http', '$q', '$location', '
     });
 
 
-
+    
     //ADD==========================================
     $scope.addPlayerStat = function () {
-        
+        /*
         $scope.loginLoading = true;
-        //$scope.myform.form_Submitted = !$scope.myform.$valid;    
+        $scope.myform.form_Submitted = !$scope.myform.$valid;    
         $scope.loginLoading = false;
         $http.post('/api/PlayerMatchStatistic/', $scope.player).success(function () {
             
@@ -52,6 +50,9 @@ app.controller('WizardPage4Controller', ['$scope', '$http', '$q', '$location', '
                 });
             }
         });
+       */
+        console.log($scope.player.mom);
+        console.log(toggleInvitet);
     };
     //ADD=========================================
 }]);
