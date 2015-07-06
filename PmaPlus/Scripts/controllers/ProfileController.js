475f70c2a9330385a10dd982eda9f37855fbff1d
Denys Arkhipenko <denys5dev@gmail.com>
2015-07-06 15:05:17 +0300

    dfd

--- PmaPlus/Scripts/controllers/ProfileController.js
+++ PmaPlus/Scripts/controllers/ProfileController.js
@@ -4,7 +4,7 @@
 app.controller('ProfileController', ['$scope', '$http', 'toaster', function ($scope, $http, toaster) {
 
 
-    $http.get('/api/TrainingTeamMembers/' + -1).success(function (result) {
+    $http.get('/api/TrainingTeamMembers/-1').success(function (result) {
         $scope.newMember = result;
     });
     
