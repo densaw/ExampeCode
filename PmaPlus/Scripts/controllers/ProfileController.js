eed93d3e29d56bac8d81ca9cbfc336ab8fcbb194
Denys Arkhipenko <denys5dev@gmail.com>
2015-07-06 16:34:00 +0300

    profile player

--- PmaPlus/Scripts/controllers/ProfileController.js
+++ PmaPlus/Scripts/controllers/ProfileController.js
@@ -86,3 +86,85 @@ app.controller('ProfileController', ['$scope', '$http', 'toaster', '$q', functio
     getProfile();
 
 }]);
+
+app.controller('PlayerProfileController', ['$scope', '$http', 'toaster', '$q', function ($scope, $http, toaster, $q) {
+
+
+    var confirmModal = angular.element('#confirmModal');
+
+    function getProfile() {
+        $http.get('/api/Player/-1').success(function (result) {
+            $scope.newPlayer = result;
+        });
+    };
+    getProfile();
+
+    $scope.save = function () {
+        confirmModal.modal('show');
+
+    };
+    $scope.cancel = function () {
+        confirmModal.modal('hide');
+
+    };
+
+
+
+    $scope.okay = function (id) {
+        $scope.loginLoading = true;
+        $scope.myform.form_Submitted = !$scope.myform.$valid;
+        //Files upload
+
+        var promises = [];
+
+
+        if ($scope.pic) {
+            $scope.loginLoading = false;
+            var fd = new FormData();
+            fd.append('file', $scope.pic);
+            var promise = $http.post('/api/Files', fd, {
+                transformRequest: angular.identity,
+                headers: { 'Content-Type': undefined }
+            })
+                .success(function (data) {
+                    $scope.newPlayer.profilePicture = data.name;
+                })
+                .error(function () {
+                    toaster.pop({
+                        type: 'error',
+                        title: 'Error',
+                        body: 'File upload ERROR!'
+                    });
+                });
+            promises.push(promise);
+        }
+        $q.all(promises).then(function () {
+            if (id != null) {
+
+                $http.put('/api/TrainingTeamMembers/' + id, $scope.newPlayer)
+                .success(function (result) {
+                    getProfile();
+                    confirmModal.modal('hide');
+                    $scope.loginLoading = false;
+                }).error(function (data, status, headers, config) {
+                    console.log(data);
+                    if (status == 400) {
+                        console.log(data);
+                        toaster.pop({
+                            type: 'error',
+                            title: 'Error', bodyOutputType: 'trustedHtml',
+                            body: 'Please complete the compulsory fields highlighted in red'
+                        });
+                    }
+                    $scope.loginLoading = false;
+                });
+
+
+            }
+
+        });
+
+    };
+    getProfile();
+
+}]);
