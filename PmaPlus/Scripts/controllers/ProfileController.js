9084be633f758cf418408887de9c0c4a0cb5d69d
Denys Arkhipenko <denys5dev@gmail.com>
2015-07-06 15:33:54 +0300

    qwqww

--- PmaPlus/Scripts/controllers/ProfileController.js
+++ PmaPlus/Scripts/controllers/ProfileController.js
@@ -4,8 +4,41 @@
 app.controller('ProfileController', ['$scope', '$http', 'toaster', function ($scope, $http, toaster) {
 
 
-    $http.get('/api/TrainingTeamMembers/-1').success(function (result) {
-        $scope.newMember = result;
-    });
-    
+    var confirmModal = angular.element('#confirmModal');
+
+    function getProfile() {
+        $http.get('/api/TrainingTeamMembers/-1').success(function (result) {
+            $scope.newMember = result;
+        });
+    };
+    getProfile();
+
+    $scope.save = function () {
+        confirmModal.modal('show');
+
+    };
+    $scope.cancel = function () {
+        confirmModal.modal('hide');
+
+    };
+
+    $scope.okay = function (id) {
+        console.log('test');
+        $scope.loginLoading = true;
+        
+        if (id != null) {
+
+            //PUT it now have no url to Update date
+            $http.put('/api/TrainingTeamMembers/' + id, $scope.newMember).success(function (result) {
+                
+                $scope.loginLoading = false;
+                confirmModal.modal('hide');
+                getProfile();
+            }).error(function (data, status, headers, config) {
+
+            });
+        }
+    };
+
+
 }]);
