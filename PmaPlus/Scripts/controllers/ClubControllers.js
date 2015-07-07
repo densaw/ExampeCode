8c62ac30aafe7d343182ee03a782a1a6e1766de0
Denis <denia24528@gmail.com>
2015-07-07 17:56:25 +0300

    images

--- PmaPlus/Scripts/controllers/ClubControllers.js
+++ PmaPlus/Scripts/controllers/ClubControllers.js
@@ -383,7 +383,7 @@ app.controller('TrainingTeamController', ['$scope', '$http', 'toaster', '$q', '$
 
 
         if ($scope.pic) {
-            $scope.loginLoading = false;
+            $scope.loginLoading = true;
             var fd = new FormData();
             fd.append('file', $scope.pic);
             var promise = $http.post('/api/Files', fd, {
@@ -1626,6 +1626,7 @@ app.controller('ClubPlayerController', ['$scope', '$http', 'toaster', '$q', '$ro
         confDelete.modal('hide');
     };
     $scope.openAdd = function () {
+        angular.element('.pma-fileupload').fileinput('clear');
         $scope.modalTitle = "Add an Player";
         $scope.newPlayer = {};
         $scope.newPlayer.userStatus = 0;
@@ -2126,6 +2127,7 @@ app.controller('CurrDetailsController', ['$scope', '$http', 'toaster', '$q', '$r
 
 
     $scope.open = function () {
+        angular.element('.pma-fileupload').fileinput('clear');
         $scope.modalTitle = 'Add Curriculum Session';
         $scope.myform.form_Submitted = false;
         $scope.newCurrDet = {};
