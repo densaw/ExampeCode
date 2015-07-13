150a3015ab4bc0974b991fa8bfbba7419f2b618a
Ihor <serkov.ihor@gmail.com>
2015-07-13 15:39:21 +0300

    owner can delete file

---
+++ PmaPlus.Data/Repository/FileOvnerRepository.cs
@@ -0,0 +1,12 @@
+﻿using PmaPlus.Data.Repository.Iterfaces;
+using PmaPlus.Model.Models;
+
+namespace PmaPlus.Data.Repository
+{
+    public class FileOvnerRepository : RepositoryBase<FileOvner>, IFileOvnerRepository
+    {
+        public FileOvnerRepository(IDatabaseFactory databaseFactory) : base(databaseFactory)
+        {
+        }
+    }
+}
